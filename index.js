394a488c77ecc6d1846bd11b4954fb35b0f590b0
Abdirabi440 <osmanabdirabi440@gmail.com>
2024-10-18 06:58:27 +0300

    complete-lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Your code here
+
+function moveDodgerLeft() {
+    const dodger = document.getElementById('dodger');
+    let leftNumbers = dodger.style.left.replace('px', '');
+    let left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 10}px`;
+    }
+  }
+  
+  function moveDodgerRight() {
+    const dodger = document.getElementById('dodger');
+    let leftNumbers = dodger.style.left.replace('px', '');
+    let left = parseInt(leftNumbers, 10);
+  
+    if (left < 360) {  // Assuming the dodger's container is 400px wide and dodger is 40px wide
+      dodger.style.left = `${left + 10}px`;
+    }
+  }
+  
